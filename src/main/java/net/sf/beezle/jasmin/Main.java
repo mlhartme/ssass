afdbcaba21afad80f3335dbfb645a4821e45e81b
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2011-11-05 15:13:10 +0000

    actually parse first sample file

--- src/main/java/net/sf/beezle/jasmin/Main.java
+++ src/main/java/net/sf/beezle/jasmin/Main.java
@@ -1,7 +1,40 @@
 package net.sf.beezle.jasmin;
 
+import de.mlhartme.mork.mapping.Mapper;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Mapper mapper;
+        int ok;
+        long tmp;
+
+        if (args.length == 0) {
+            System.out.println("CSS Parser");
+            System.out.println("usage: Main <filename>+");
+        } else {
+            mapper = load();
+            tmp = System.currentTimeMillis();
+            ok = 0;
+            for (String name : args) {
+                System.out.println(name + ":");
+                if (mapper.run(name) != null) {
+                    ok++;
+                }
+            }
+            System.out.println(ok + "/" + args.length + " parsed successfully.");
+            System.out.println((System.currentTimeMillis() - tmp) + " ms");
+            System.exit(args.length - ok);
+        }
+    }
+
+    private static Mapper load() {
+        Mapper mapper;
+        long tmp;
+
+        System.out.print("loading mapper ... ");
+        tmp = System.currentTimeMillis();
+        mapper = new Mapper("jasmin.Mapper");
+        System.out.println("done (" + (System.currentTimeMillis() - tmp) + " ms)");
+        return mapper;
     }
 }
