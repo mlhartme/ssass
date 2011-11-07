eb1292696794ece3a4ed072883fb0927229d8410
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2011-11-07 15:27:27 +0000

    add prio

--- src/main/java/net/sf/beezle/jasmin/scss/Declaration.java
+++ src/main/java/net/sf/beezle/jasmin/scss/Declaration.java
@@ -3,9 +3,11 @@ package net.sf.beezle.jasmin.scss;
 public class Declaration {
     private final String property;
     private final Expr expr;
+    private final Prio prio;
 
-    public Declaration(String property, Expr expr) {
+    public Declaration(String property, Expr expr, Prio prio) {
         this.property = property;
         this.expr = expr;
+        this.prio = prio;
     }
 }
