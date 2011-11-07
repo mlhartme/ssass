b5e57cadb7f4f49b3890e450370c18c441ee6210
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2011-11-07 15:52:29 +0000

    mapping complete

--- src/main/java/net/sf/beezle/jasmin/scss/Pseudo.java
+++ src/main/java/net/sf/beezle/jasmin/scss/Pseudo.java
@@ -1,9 +1,17 @@
 package net.sf.beezle.jasmin.scss;
 
+import net.sf.beezle.jasmin.scss.term.Strng;
+
 public class Pseudo extends BaseSelector {
+    private final String function;
     private final String name;
 
     public Pseudo(String name) {
+        this(null, name);
+    }
+
+    public Pseudo(String function, String name) {
+        this.function = function;
         this.name = name;
     }
 }
--- src/main/java/net/sf/beezle/jasmin/scss/term/Function.java
+++ src/main/java/net/sf/beezle/jasmin/scss/term/Function.java
@@ -3,9 +3,11 @@ package net.sf.beezle.jasmin.scss.term;
 import net.sf.beezle.jasmin.scss.Expr;
 
 public class Function extends BaseTerm {
+    private final String name;
     private final Expr expr;
 
-    public Function(Expr expr) {
+    public Function(String name, Expr expr) {
+        this.name = name;
         this.expr = expr;
     }
 }
