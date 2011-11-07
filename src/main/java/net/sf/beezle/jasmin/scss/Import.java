57a5ab40e3a6cfeba8cddb708097b4ebf2c0b7ed
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2011-11-07 15:04:39 +0000

    semantics fixes

--- src/main/java/net/sf/beezle/jasmin/scss/Import.java
+++ src/main/java/net/sf/beezle/jasmin/scss/Import.java
@@ -1,11 +1,9 @@
 package net.sf.beezle.jasmin.scss;
 
-import net.sf.beezle.sushi.util.Strings;
-
 public class Import {
     private final String[] mediaList;
 
-    public Import(String mediaListHead, String[] mediaListTail) {
-        this.mediaList = Strings.cons(mediaListHead, mediaListTail);
+    public Import(String[] mediaList) {
+        this.mediaList = mediaList;
     }
 }
--- src/main/java/net/sf/beezle/jasmin/scss/Media.java
+++ src/main/java/net/sf/beezle/jasmin/scss/Media.java
@@ -1,9 +1,11 @@
 package net.sf.beezle.jasmin.scss;
 
 public class Media extends Statement {
+    private final String[] mediaList;
     private final Ruleset[] rulesets;
 
-    public Media(Ruleset[] rulesets) {
+    public Media(String[] mediaList, Ruleset[] rulesets) {
+        this.mediaList = mediaList;
         this.rulesets = rulesets;
     }
 }
